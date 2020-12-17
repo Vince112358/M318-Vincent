a48c1bdbb4cdb50e501179635671e7fa7bca4d76
Vincent Birrer <Vincent_Birrer@sluz.ch>
2020-12-17 15:55:04 +0100

    First Commit

---
+++ SSV-Application/main.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace SSV_Application
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+			this.label1 = new System.Windows.Forms.Label();
+			this.gbNeueVerbindung = new System.Windows.Forms.GroupBox();
+			this.lblPfeil = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.btnStartUndEnde = new System.Windows.Forms.Button();
+			this.btnAb = new System.Windows.Forms.Button();
+			this.cbZielort = new System.Windows.Forms.ComboBox();
+			this.cbAbfahrtsort = new System.Windows.Forms.ComboBox();
+			this.lblAktiverModus = new System.Windows.Forms.Label();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.lblZielort = new System.Windows.Forms.Label();
+			this.gbSucheEinschraenken = new System.Windows.Forms.GroupBox();
+			this.dtpZeit = new System.Windows.Forms.DateTimePicker();
+			this.lblAb = new System.Windows.Forms.Label();
+			this.dtpDatum = new System.Windows.Forms.DateTimePicker();
+			this.lblAbfahrtsort = new System.Windows.Forms.Label();
+			this.lblMode = new System.Windows.Forms.Label();
+			this.btnStationFinden = new System.Windows.Forms.Button();
+			this.btnAnzeigen = new System.Windows.Forms.Button();
+			this.btnTeilen = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.cbVerbindungen = new System.Windows.Forms.ComboBox();
+			this.dgvAuflisten = new System.Windows.Forms.DataGridView();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.cbStationAnzeigen = new System.Windows.Forms.ComboBox();
+			this.gbNeueVerbindung.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.gbSucheEinschraenken.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvAuflisten)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// gbNeueVerbindung
+			// 
+			this.gbNeueVerbindung.BackColor = System.Drawing.Color.Silver;
+			this.gbNeueVerbindung.Controls.Add(this.lblPfeil);
+			this.gbNeueVerbindung.Controls.Add(this.groupBox1);
+			this.gbNeueVerbindung.Controls.Add(this.cbZielort);
+			this.gbNeueVerbindung.Controls.Add(this.cbAbfahrtsort);
+			this.gbNeueVerbindung.Controls.Add(this.lblAktiverModus);
+			this.gbNeueVerbindung.Controls.Add(this.btnGo);
+			this.gbNeueVerbindung.Controls.Add(this.lblZielort);
+			this.gbNeueVerbindung.Controls.Add(this.gbSucheEinschraenken);
+			this.gbNeueVerbindung.Controls.Add(this.lblAbfahrtsort);
+			this.gbNeueVerbindung.Controls.Add(this.lblMode);
+			resources.ApplyResources(this.gbNeueVerbindung, "gbNeueVerbindung");
+			this.gbNeueVerbindung.Name = "gbNeueVerbindung";
+			this.gbNeueVerbindung.TabStop = false;
+			// 
+			// lblPfeil
+			// 
+			resources.ApplyResources(this.lblPfeil, "lblPfeil");
+			this.lblPfeil.Name = "lblPfeil";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.btnStartUndEnde);
+			this.groupBox1.Controls.Add(this.btnAb);
+			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.TabStop = false;
+			// 
+			// btnStartUndEnde
+			// 
+			resources.ApplyResources(this.btnStartUndEnde, "btnStartUndEnde");
+			this.btnStartUndEnde.Name = "btnStartUndEnde";
+			this.btnStartUndEnde.UseVisualStyleBackColor = true;
+			this.btnStartUndEnde.Click += new System.EventHandler(this.btnStartUndEnde_Click);
+			// 
+			// btnAb
+			// 
+			resources.ApplyResources(this.btnAb, "btnAb");
+			this.btnAb.Name = "btnAb";
+			this.btnAb.UseVisualStyleBackColor = true;
+			this.btnAb.Click += new System.EventHandler(this.btnAb_Click);
+			// 
+			// cbZielort
+			// 
+			this.cbZielort.FormattingEnabled = true;
+			resources.ApplyResources(this.cbZielort, "cbZielort");
+			this.cbZielort.Name = "cbZielort";
+			this.cbZielort.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Vorschlaege_KeyUp);
+			// 
+			// cbAbfahrtsort
+			// 
+			this.cbAbfahrtsort.FormattingEnabled = true;
+			resources.ApplyResources(this.cbAbfahrtsort, "cbAbfahrtsort");
+			this.cbAbfahrtsort.Name = "cbAbfahrtsort";
+			this.cbAbfahrtsort.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Vorschlaege_KeyUp);
+			// 
+			// lblAktiverModus
+			// 
+			resources.ApplyResources(this.lblAktiverModus, "lblAktiverModus");
+			this.lblAktiverModus.Name = "lblAktiverModus";
+			// 
+			// btnGo
+			// 
+			resources.ApplyResources(this.btnGo, "btnGo");
+			this.btnGo.Name = "btnGo";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+			// 
+			// lblZielort
+			// 
+			resources.ApplyResources(this.lblZielort, "lblZielort");
+			this.lblZielort.Name = "lblZielort";
+			// 
+			// gbSucheEinschraenken
+			// 
+			this.gbSucheEinschraenken.Controls.Add(this.dtpZeit);
+			this.gbSucheEinschraenken.Controls.Add(this.lblAb);
+			this.gbSucheEinschraenken.Controls.Add(this.dtpDatum);
+			resources.ApplyResources(this.gbSucheEinschraenken, "gbSucheEinschraenken");
+			this.gbSucheEinschraenken.Name = "gbSucheEinschraenken";
+			this.gbSucheEinschraenken.TabStop = false;
+			// 
+			// dtpZeit
+			// 
+			resources.ApplyResources(this.dtpZeit, "dtpZeit");
+			this.dtpZeit.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dtpZeit.Name = "dtpZeit";
+			this.dtpZeit.ShowUpDown = true;
+			// 
+			// lblAb
+			// 
+			resources.ApplyResources(this.lblAb, "lblAb");
+			this.lblAb.Name = "lblAb";
+			// 
+			// dtpDatum
+			// 
+			resources.ApplyResources(this.dtpDatum, "dtpDatum");
+			this.dtpDatum.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dtpDatum.MinDate = new System.DateTime(2020, 12, 1, 0, 0, 0, 0);
+			this.dtpDatum.Name = "dtpDatum";
+			// 
+			// lblAbfahrtsort
+			// 
+			resources.ApplyResources(this.lblAbfahrtsort, "lblAbfahrtsort");
+			this.lblAbfahrtsort.Name = "lblAbfahrtsort";
+			// 
+			// lblMode
+			// 
+			resources.ApplyResources(this.lblMode, "lblMode");
+			this.lblMode.Name = "lblMode";
+			// 
+			// btnStationFinden
+			// 
+			resources.ApplyResources(this.btnStationFinden, "btnStationFinden");
+			this.btnStationFinden.Name = "btnStationFinden";
+			this.btnStationFinden.UseVisualStyleBackColor = true;
+			this.btnStationFinden.Click += new System.EventHandler(this.btnStationFinden_Click);
+			// 
+			// btnAnzeigen
+			// 
+			resources.ApplyResources(this.btnAnzeigen, "btnAnzeigen");
+			this.btnAnzeigen.Name = "btnAnzeigen";
+			this.btnAnzeigen.UseVisualStyleBackColor = true;
+			this.btnAnzeigen.Click += new System.EventHandler(this.btnAufKarteAnzeigen_Click);
+			// 
+			// btnTeilen
+			// 
+			resources.ApplyResources(this.btnTeilen, "btnTeilen");
+			this.btnTeilen.Name = "btnTeilen";
+			this.btnTeilen.UseVisualStyleBackColor = true;
+			this.btnTeilen.Click += new System.EventHandler(this.btnTeilen_Click);
+			// 
+			// label5
+			// 
+			resources.ApplyResources(this.label5, "label5");
+			this.label5.Name = "label5";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.Color.Silver;
+			this.groupBox2.Controls.Add(this.cbVerbindungen);
+			this.groupBox2.Controls.Add(this.btnTeilen);
+			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.TabStop = false;
+			// 
+			// cbVerbindungen
+			// 
+			this.cbVerbindungen.FormattingEnabled = true;
+			resources.ApplyResources(this.cbVerbindungen, "cbVerbindungen");
+			this.cbVerbindungen.Name = "cbVerbindungen";
+			// 
+			// dgvAuflisten
+			// 
+			this.dgvAuflisten.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvAuflisten.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			resources.ApplyResources(this.dgvAuflisten, "dgvAuflisten");
+			this.dgvAuflisten.Name = "dgvAuflisten";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.BackColor = System.Drawing.Color.Silver;
+			this.groupBox3.Controls.Add(this.label3);
+			this.groupBox3.Controls.Add(this.label2);
+			this.groupBox3.Controls.Add(this.cbStationAnzeigen);
+			this.groupBox3.Controls.Add(this.btnAnzeigen);
+			this.groupBox3.Controls.Add(this.btnStationFinden);
+			resources.ApplyResources(this.groupBox3, "groupBox3");
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.TabStop = false;
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.Name = "label3";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// cbStationAnzeigen
+			// 
+			this.cbStationAnzeigen.FormattingEnabled = true;
+			resources.ApplyResources(this.cbStationAnzeigen, "cbStationAnzeigen");
+			this.cbStationAnzeigen.Name = "cbStationAnzeigen";
+			this.cbStationAnzeigen.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Vorschlaege_KeyUp);
+			// 
+			// main
+			// 
+			this.AcceptButton = this.btnGo;
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.dgvAuflisten);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.gbNeueVerbindung);
+			this.Controls.Add(this.label1);
+			this.MaximizeBox = false;
+			this.Name = "main";
+			this.ShowIcon = false;
+			this.gbNeueVerbindung.ResumeLayout(false);
+			this.gbNeueVerbindung.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.gbSucheEinschraenken.ResumeLayout(false);
+			this.gbSucheEinschraenken.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dgvAuflisten)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbNeueVerbindung;
+        private System.Windows.Forms.GroupBox gbSucheEinschraenken;
+        private System.Windows.Forms.Button btnAnzeigen;
+        private System.Windows.Forms.Button btnTeilen;
+        private System.Windows.Forms.Button btnAb;
+        private System.Windows.Forms.Button btnStartUndEnde;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnStationFinden;
+        private System.Windows.Forms.Label lblZielort;
+        private System.Windows.Forms.Label lblAbfahrtsort;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.Label lblAktiverModus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDatum;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbVerbindungen;
+        private System.Windows.Forms.Label lblAb;
+        private System.Windows.Forms.DateTimePicker dtpZeit;
+        private System.Windows.Forms.DataGridView dgvAuflisten;
+        private System.Windows.Forms.ComboBox cbZielort;
+        private System.Windows.Forms.ComboBox cbAbfahrtsort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbStationAnzeigen;
+        private System.Windows.Forms.Label lblPfeil;
+    }
+}
+
---
+++ SSV-Application/main.cs
@@ -0,0 +1,339 @@
+﻿using SwissTransport;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSV_Application
+{
+    public partial class main : Form
+    {
+        Transport transport = new Transport();
+
+        public main()
+        {
+            InitializeComponent();
+            ButtonsStartUp();
+        }
+
+
+        private void btnAb_Click(object sender, EventArgs e)
+        {
+            // Button deaktiveren, um doppeltes ausführen zu verhindern. Modus umschalten.
+            btnStartUndEnde.Enabled = true;
+            btnAb.Enabled = false;
+            cbZielort.Text = "";
+            lblAktiverModus.Text = "Ab (Abfahrtstafel)";
+
+            //Die Zielorte deaktivieren
+            lblZielort.Enabled = false;
+            cbZielort.Enabled = false;
+            lblPfeil.Enabled = false;
+
+            //Teilen-Funktion deaktivieren, da diese nur im normalen Modus funktionieren soll.
+            cbVerbindungen.Enabled = false;
+            btnTeilen.Enabled = false;
+            cbVerbindungen.Text = "";
+
+            //Eingabe von Datum und Uhrzeit bei der Abfahrtstabelle verhindern
+            dtpDatum.Enabled = false;
+            dtpZeit.Enabled = false;
+            lblAb.Enabled = false;
+        }
+
+        private void btnStartUndEnde_Click(object sender, EventArgs e)
+        {
+            //Felder für die Standard-Verbindungssuche aktivieren
+            btnAb.Enabled = true;
+            lblZielort.Enabled = true;
+            cbZielort.Enabled = true;
+            lblPfeil.Enabled = true;
+
+            //Modus umschalten
+            btnStartUndEnde.Enabled = false;
+            lblAktiverModus.Text = "Start und Ende";
+
+            //Sicherstellen, dass die Teilen-funktion aktiviert ist.
+            cbVerbindungen.Enabled = true;
+            btnTeilen.Enabled = true;
+
+            //Eingabe von Datum und Uhrzeit ermöglichen
+            dtpDatum.Enabled = true;
+            dtpZeit.Enabled = true;
+            lblAb.Enabled = true;
+        }
+
+        private void ButtonsStartUp()
+        {
+            btnStartUndEnde.Enabled = false;
+            lblAktiverModus.Text = "Start und Ende";
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connections = transport.GetConnections(cbAbfahrtsort.Text, cbZielort.Text, dtpDatum.Value.ToString("yyyy-MM-dd"), dtpZeit.Text);
+                var StationBoard = transport.GetStationBoard(cbAbfahrtsort.Text, "");
+
+                cbVerbindungen.Items.Clear();
+                cbAbfahrtsort.Items.Clear();
+                cbZielort.Items.Clear();
+
+
+                if (btnAb.Enabled) //Modus "Start und Ende" ist aktiv
+                {
+                    clearDGV();
+
+                    DataTable verbindungen = new DataTable();
+                    verbindungen.Columns.Add("Von");
+                    verbindungen.Columns.Add("Abfahrt");
+                    verbindungen.Columns.Add("Gleis");
+                    verbindungen.Columns.Add("Nach");
+                    verbindungen.Columns.Add("Ankunft");
+                    verbindungen.Columns.Add("Datum");
+
+                    if (cbAbfahrtsort.Text != string.Empty && cbZielort.Text != string.Empty) //Überprüft, ob eingabe vorhanden
+                    {
+
+                        foreach (Connection connection in connections.ConnectionList)
+                        {
+                            string Abfahrtszeit = DateTime.Parse(connection.From.Departure).ToString("HH:mm");
+                            string Ankunftszeit = DateTime.Parse(connection.To.Arrival).ToString("HH:mm");
+                            string DatumDerVerbindung = DateTime.Parse(dtpDatum.Text).ToString("dd.MM.yyyy");
+
+                            verbindungen.Rows.Add(connection.From.Station.Name, Abfahrtszeit, connection.From.Platform, connection.To.Station.Name, Ankunftszeit, DatumDerVerbindung);
+
+                            cbVerbindungen.Items.Add(Abfahrtszeit + " ab " + connection.From.Station.Name + " (Gleis " + connection.From.Platform + "). Ankunft in " + connection.To.Station.Name + " um " + Ankunftszeit + " am " + DatumDerVerbindung);
+                        }
+                        dgvAuflisten.DataSource = verbindungen;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bitte Abfahrtsort und Zielort eingeben.");
+                    }
+                }
+                else if (btnStartUndEnde.Enabled) //Modus "Ab" ist aktiv (Abfahrtstafel)
+                {
+                    clearDGV();
+
+                    DataTable abfahrplan = new DataTable();
+                    abfahrplan.Columns.Add("Typ");
+                    abfahrplan.Columns.Add("Von");
+                    abfahrplan.Columns.Add("Nach");
+                    abfahrplan.Columns.Add("Abfahrt");
+
+
+                    if (cbAbfahrtsort.Text != string.Empty) //Überprüft, ob eingabe vorhanden
+                    {
+                        transport.GetStationBoard(cbAbfahrtsort.Text, "0");
+
+                        foreach (StationBoard verbindung in StationBoard.Entries)
+                        {
+                            string typ = verbindung.Name.Substring(0, 2); //Nur der Typ des Zuges ausgeben (z.B. RE oder S1)
+                            string stunde = "";
+                            string minute = "";
+                            if (verbindung.Stop.Departure.Hour < 10) //Falls einstellig wird vorne eine 0 hinzugefügt
+                            {
+                                stunde = "0" + Convert.ToString(verbindung.Stop.Departure.Hour);
+                            }
+                            else
+                            {
+                                stunde = Convert.ToString(verbindung.Stop.Departure.Hour);
+                            }
+
+                            if (verbindung.Stop.Departure.Minute < 10) //Uhrzeit formatieren
+                            {
+                                minute = "0" + Convert.ToString(verbindung.Stop.Departure.Minute);
+                            }
+                            else
+                            {
+                                minute = Convert.ToString(verbindung.Stop.Departure.Minute);
+                            }
+
+                            string Abfahrtszeit = stunde + ":" + minute;
+
+                            abfahrplan.Rows.Add(typ, StationBoard.Station.Name, verbindung.To, Abfahrtszeit);
+
+                        }
+
+                        dgvAuflisten.DataSource = abfahrplan;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bitte Namen der Station eingeben.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ein Fehler beim generieren der Abfahrten ist aufgetretten.\n ID 500");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ein Fehler ist aufgetretten! \nHast du alle nötigen Eingaben ausgefüllt?", "Fehler beim Generieren", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Create_GmapStation(string x, string y) //Link aus den Koordinaten zusammensetzen und auf Google Maps öffnen
+        {
+            string url = "https://www.google.ch/maps/place/" + x + "," + y;
+            System.Diagnostics.Process.Start(url);
+        }
+
+        private void btnAufKarteAnzeigen_Click(object sender, EventArgs e)
+        {
+            cbStationAnzeigen.Items.Clear();
+
+            if (cbStationAnzeigen.Text != string.Empty)
+            {
+                Stations stations = transport.GetStations(cbStationAnzeigen.Text);
+                Station station = stations.StationList[0];
+                /*
+                Create_GmapStation(Convert.ToString(station.Coordinate.XCoordinate).Replace(',', '.'), Convert.ToString(station.Coordinate.YCoordinate).Replace(',', '.'));
+                */
+
+                Map map = new Map(station.Coordinate.XCoordinate, station.Coordinate.YCoordinate);
+                map.Show();
+            }
+            else
+            {
+                MessageBox.Show("Bitte zuerst eine Verbindung eingeben.");
+            }
+
+        }
+
+        private void btnTeilen_Click(object sender, EventArgs e)
+        {
+            if (cbVerbindungen.SelectedItem != null)
+            {
+                try //Email öffnen und abfüllen
+                {
+                    System.Diagnostics.Process.Start("mailto:" + "?subject=Meine Verbindung" + "&body= Hallo, folgt sende ich dir meine Verbindung: " + cbVerbindungen.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Ein Fehler beim generieren der Email ist aufgetretten. Hast du eine passende MailingApp installiert?", "Fehler beim Generieren", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte zuerst Auswahl deiner Verbindung treffen");
+            }
+
+        }
+
+        private void btnStationFinden_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Diese Funktion ist noch in Bearbeitung. Möchtest du inzwischen die nächste Station auf Google Maps suchen?", "Noch in Bearbeitung", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("https://www.google.ch/maps/dir///@47.0512613,8.3053424,15z/data=!4m2!4m1!3e3");
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void clearDGV()
+        {
+            dgvAuflisten.DataSource = null;
+            dgvAuflisten.Columns.Clear();
+            dgvAuflisten.Rows.Clear();
+        }
+
+        private void Get_Stations_Abfahrt(string eingabe)
+        {
+            if (eingabe.Length >= 2)
+            {
+                cbAbfahrtsort.Items.Clear();
+                Stations stations = transport.GetStations(eingabe);
+                foreach (Station station in stations.StationList)
+                {
+                    cbAbfahrtsort.Items.Add(station.Name);
+                    cbAbfahrtsort.BringToFront();
+                }
+            }
+        }
+
+        public List<Station> Vorschlaege(string texteingabe) //Vorschläge laden mit GetStations, welche folgend mit "vorschlaege" aufgerufen werden können
+        {
+            var stationsobjekte = transport.GetStations(query: texteingabe);
+
+            List<Station> stationenvorschlaege = stationsobjekte.StationList;
+
+            return stationenvorschlaege;
+        }
+
+        private void Vorschlaege_KeyUp(object sender, KeyEventArgs e) //Intelisense für cbZielort
+        {
+            EingabeVorschlag intelisense = new EingabeVorschlag();
+            if (((ComboBox)sender).Text.Length >= 3)
+            {
+                if (e.KeyCode != Keys.Escape)
+                {
+                    if (e.KeyCode != Keys.Down && e.KeyCode != Keys.Up && e.KeyCode != Keys.Enter && e.KeyCode != Keys.Left && e.KeyCode != Keys.Right)
+                    {
+                        intelisense.AutoCompletion((ComboBox)sender);
+                    }
+                }
+                else
+                {
+                    ((ComboBox)sender).DroppedDown = false;
+                }
+            }
+        }
+
+        public class EingabeVorschlag
+        {
+            Transport transport = new Transport();
+            public void AutoCompletion(ComboBox sender)
+            {
+                while (sender.Items.Count > 0)
+                {
+                    sender.Items.RemoveAt(0);
+                }
+
+                Cursor.Current = Cursors.WaitCursor;
+
+                List<string> stations = GetSuggestions(sender.Text);
+
+                Cursor.Current = Cursors.WaitCursor;
+
+                foreach (String station in stations)
+                {
+                    if (station != null)
+                    {
+                        sender.Items.Add(station);
+                    }
+                }
+                sender.DroppedDown = true;
+            }
+
+            public List<string> GetSuggestions(string query)
+            {
+                List<string> stationsList = new List<string>(); //alle stationen, welche dem string entsprechen
+                var stations = transport.GetStations(query);
+
+                foreach (Station station in stations.StationList)
+                {
+                    stationsList.Add(station.Name); //station wird zur combobox hinzugefügt
+                }
+                if (stationsList.Count == 0)
+                {
+                    stationsList.Add("Kein Resultat");
+                }
+                return stationsList;
+            }
+        }
+    }
+}
